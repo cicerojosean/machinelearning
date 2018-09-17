2146282254aff04e3a131a480086a87294cc8823
cicerojosean <cicerojosean@yahoo.com.br>
2018-09-17 12:41:16 -0300

    fdf

--- ect2702-ml/Unidade 1/02 - Perceptron/perceptron4.py
+++ ect2702-ml/Unidade 1/02 - Perceptron/perceptron4.py
@@ -1,7 +1,7 @@
 """
 Created on Mon Aug 27 15:31:13 2018
 
-@author: cicero
+Aluno: Cícero Josean
 """
 
 import numpy as np
@@ -9,41 +9,61 @@ import numpy as np
 entradas  = np.array([[0,0,1],[1,1,0]])  #vetor de entrada de dados
 saidas = np.array([-1,1])    #vetor de saida de dados
 pesos = np.array([0.4,-0.6,0.6])   #pesos de cada variavel de entrada
+erro = np.array([0,0,0])
 taxaAprendizagem = 0.4
 limiar = 0.5
-novas_entradas  = np.array([[1,1,1],[0,0,0],[0,1,1]])  #valores para gerar a saída
+novas_entradas  = np.array([[1,1,1],[0,0,0],[1,0,0],[0,1,1]])  #valores para gerar a saída
 epocas = 100  #define o numero de épocas que serão utilizadas
 
 def stepFunction(soma):
-    if(soma>=1):
+    if(soma>=0):            
+        print("Saida depois da função de ativação: 1")
         return 1
-    return 0
+    print("Saida depois da função de ativação: -1")
+    return -1
 
 #   calcula a saida com pesos atuais (entrada * pesos)
 def calculaSaida(registro):   
-    s = registro.dot(pesos)+limiar
+    s = registro.dot(pesos)-limiar
+    print("Saida antes da função de ativação: ",round(s,2))
     return stepFunction(s)
 
 #encontrar o conjunto de pesos que irão satisfazer o conjunto de dados
 def treinar(limiar):
-    for e in range(epocas):
+    print("\n________Treinamento__________")
+    for e in range(epocas):  
+        print("\nÉpoca ",e)
         for i in range (len(saidas)):
             saidaCalculada = calculaSaida(entradas[i])
-            erro = (saidas[i] - saidaCalculada)
+            erro[i] = (saidas[i] - saidaCalculada)
             #atualizacao dos pesos
-            pesos[:] = pesos[:] + (taxaAprendizagem*entradas[i]*erro) 
+            pesos[:] = pesos[:] + (taxaAprendizagem*entradas[i]*erro[i]) 
             print("Peso atualizado: "+ str(pesos[:]))
             #atualização do BIAS(limiar)
-            limiar = limiar + (taxaAprendizagem*erro) 
-        
-treinar(limiar)
-print("Rede Neural Treinada")
+            limiar = limiar + (taxaAprendizagem*(-1)*erro[i]) 
+            print("Limiar atualizado = ",limiar)
+            print("erro = ",erro[:])
+        if(all(erro[:]==0)):  #erro igual a zero
+            break
 
-print(calculaSaida(entradas[0]))
-print(calculaSaida(entradas[1]))
-print(calculaSaida(novas_entradas[0]))
-print(calculaSaida(novas_entradas[1]))
-print(calculaSaida(novas_entradas[2]))
+print("\nPrograma para implementar um perceptron e mostrar os resultados")     
+treinar(limiar)
+print("\n_______Rede Neural Treinada________")
+
+print("\nEntradas Dadas:")
+print("\nEntrada ",entradas[0])
+calculaSaida(entradas[0])
+print("\nEntrada ",entradas[1])
+calculaSaida(entradas[1])
+print("\nEntradas Novas:")
+print("\nEntrada ",novas_entradas[0])
+calculaSaida(novas_entradas[0])
+print("\nEntrada ",novas_entradas[1])
+calculaSaida(novas_entradas[1])
+print("\nEntrada ",novas_entradas[2])
+calculaSaida(novas_entradas[2])
+print("\nEntrada ",novas_entradas[3])
+calculaSaida(novas_entradas[3])
 
 
 
